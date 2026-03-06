fed570802ce899ae2a5166c9050d4a6778f856a3
Yuki-eng924 <yukichannel0720@icloud.com>
2026-03-06 19:51:24 +0900

    機体下　超音波　テストコード

---
+++ Echo_test/Echo_test.ino
@@ -0,0 +1,26 @@
+#include "Echo.h"
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  
+  Serial.begin(9600);
+
+  //超音波センサを初期化
+  echo_init();
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  read_echo();
+
+  //シリアルで超音波高度を吐き出す
+  Serial.print("Altitude: ");
+  Serial.print(data_under_urm_altitude_m);
+  Serial.println(" m");
+  delay(100);
+
+}
